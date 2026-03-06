e3cfb398431978dd85b0cc0d445b79a8879d8c08
joneikholm <joneikholm@hotmail.com>
2026-03-06 14:23:53 +0100

    Jons Component

---
+++ components/jonsComponent.js
@@ -0,0 +1,26 @@
+// koden kommer her
+import {View, Text, Image, StyleSheet} from 'react-native'
+export function jonsComponent() {
+    return(
+     <View style={styles.container}>
+         <Image source={require('../assets/jon_piano.png')}
+            style={styles.image} />
+        <Text style={styles.text}>Play or get get played!</Text>
+     </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width:'100%'
+    },  
+    text:{
+        fontSize:20,
+        color:"blue",
+        textAlign:'center'
+    },
+    image:{
+        width:'100%',
+        height:300
+    }
+})
