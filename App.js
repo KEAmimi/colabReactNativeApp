c318b8c7751b793c90b893cf2af281bd6e76427c
mimi0001 <mimi0001@stud.ek.dk>
2026-03-06 13:46:02 +0100

    Added Mikkels Page

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { MikkelsPage } from "./components/mikkelsComponent";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,16 +16,28 @@ export default function App() {
           screenOptions={{ headerShown: true }}
         >
           <Stack.Screen name="LandingPage" component={LandingPage} />
+          <Stack.Screen name="MikkelsPage" component={MikkelsPage} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
 
-function LandingPage() {
+function LandingPage({ navigation, route }) {
+  function navigateTo(page) {
+    navigation.navigate(page);
+  }
+
   return (
     <View>
       <Text>Dette er landing pagen for vores collaborative app</Text>
+      <Pressable
+        onPress={() => {
+          navigateTo("MikkelsPage");
+        }}
+      >
+        <Text>MikkelsPage</Text>
+      </Pressable>
     </View>
   );
 }
---
+++ components/mikkelsComponent.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function MikkelsPage() {
+  return (
+    <View>
+      <Text>Dette er Mikkels side</Text>
+    </View>
+  );
+}
